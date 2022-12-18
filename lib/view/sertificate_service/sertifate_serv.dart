23a66e32ab40ba345365f08925b3ac18370d512e
githubsobir <githubsobir@gmail.com>
2022-12-18 18:31:39 +0500

    mac

---
+++ lib/view/sertificate_service/sertifate_serv.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SertificateServices extends StatefulWidget {
+  const SertificateServices({Key? key}) : super(key: key);
+
+  @override
+  State<SertificateServices> createState() => _SertificateServicesState();
+}
+
+class _SertificateServicesState extends State<SertificateServices> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/view/sertificate_service/service_page_sert/provider_sertificates.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/foundation.dart';
+
+class ProviderCertificateService extends ChangeNotifier{
+
+
+  Future getDateServiceCertificate()async{
+
+  }
+
+
+
+}
