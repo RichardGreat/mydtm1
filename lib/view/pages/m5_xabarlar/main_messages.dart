d91ffdbae12ece891f9ab22ce432cb38c90605fc
uzbmb <githubsobir@gmail.com>
2024-02-27 19:29:01 +0500

    yangiliklar xabarnoma qavti to'g'irlandi

--- lib/view/pages/m5_xabarlar/main_messages.dart
+++ lib/view/pages/m5_xabarlar/main_messages.dart
@@ -337,116 +337,121 @@ class _MainMessagesState extends State<MainMessages> {
                             ),
                           ),
                         ),
-                        SafeArea(
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: ListView.builder(
-                                itemCount: listModelSocket.length,
-                                itemBuilder: (context, index) => Container(
-                                      margin: const EdgeInsets.only(
-                                          right: 4, left: 5, bottom: 8, top: 3),
-                                      decoration: BoxDecoration(
-                                          borderRadius:
-                                              BorderRadius.circular(8),
-                                          color: MyColors.appColorWhite(),
-                                          boxShadow: [
-                                            BoxShadow(
-                                                color: Colors.teal
-                                                    .withOpacity(0.3),
-                                                spreadRadius: 1,
-                                                blurRadius: 0.5)
-                                          ]),
-                                      child: Column(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.center,
-                                        children: [
-                                          Row(
-                                            crossAxisAlignment:
-                                                CrossAxisAlignment.center,
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.spaceBetween,
-                                            children: [
-                                              Padding(
-                                                padding: const EdgeInsets.only(
-                                                    left: 10,
-                                                    top: 8,
-                                                    bottom: 7),
-                                                child: Row(
-                                                  children: [
-                                                    Image.asset(
-                                                      "assets/images/logobba.png",
-                                                      height: 30,
-                                                    ),
-                                                  ],
+                  RefreshIndicator.adaptive(
+                    onRefresh: ()async{
+                    await  getData();
+                    },
+                          child: SafeArea(
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: ListView.builder(
+                                  itemCount: listModelSocket.length,
+                                  itemBuilder: (context, index) => Container(
+                                        margin: const EdgeInsets.only(
+                                            right: 4, left: 5, bottom: 8, top: 3),
+                                        decoration: BoxDecoration(
+                                            borderRadius:
+                                                BorderRadius.circular(8),
+                                            color: MyColors.appColorWhite(),
+                                            boxShadow: [
+                                              BoxShadow(
+                                                  color: Colors.teal
+                                                      .withOpacity(0.3),
+                                                  spreadRadius: 1,
+                                                  blurRadius: 0.5)
+                                            ]),
+                                        child: Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.center,
+                                          children: [
+                                            Row(
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.center,
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.spaceBetween,
+                                              children: [
+                                                Padding(
+                                                  padding: const EdgeInsets.only(
+                                                      left: 10,
+                                                      top: 8,
+                                                      bottom: 7),
+                                                  child: Row(
+                                                    children: [
+                                                      Image.asset(
+                                                        "assets/images/logobba.png",
+                                                        height: 30,
+                                                      ),
+                                                    ],
+                                                  ),
                                                 ),
-                                              ),
-                                              Padding(
-                                                padding: const EdgeInsets.only(
-                                                    right: 10,
-                                                    top: 5,
-                                                    bottom: 5),
-                                                child: Text(
-                                                    listModelSocket[index]
-                                                        .sendDate
-                                                        .toString(),
-                                                    style: const TextStyle(
-                                                        fontWeight:
-                                                            FontWeight.w600)),
-                                              ),
-                                            ],
-                                          ),
-
-                                          SizedBox(
-                                            width: MediaQuery.of(context).size.width*0.95,
-                                            child: ExpansionTile(
-                                                shape: Border.all(color: Colors.transparent),
-                                              title: Text(listModelSocket[index].title, style: const TextStyle(fontWeight: FontWeight.bold),),
-                                              children:[ Padding(
-                                                padding: const EdgeInsets.all(8.0),
-                                                child: Text(listModelSocket[index].summary),
-                                              ),]
-
+                                                Padding(
+                                                  padding: const EdgeInsets.only(
+                                                      right: 10,
+                                                      top: 5,
+                                                      bottom: 5),
+                                                  child: Text(
+                                                      listModelSocket[index]
+                                                          .sendDate
+                                                          .toString(),
+                                                      style: const TextStyle(
+                                                          fontWeight:
+                                                              FontWeight.w600)),
+                                                ),
+                                              ],
                                             ),
-                                          ),
-                                          // Padding(
-                                          //   padding:
-                                          //   const EdgeInsets.fromLTRB(
-                                          //       8, 8, 8, 10),
-                                          //   child: Row(
-                                          //     crossAxisAlignment:
-                                          //     CrossAxisAlignment.center,
-                                          //     mainAxisAlignment:
-                                          //     MainAxisAlignment
-                                          //         .spaceBetween,
-                                          //     children: [
-                                          //       const Icon(
-                                          //         Icons.keyboard_arrow_down,
-                                          //       ),
-                                          //       Row(
-                                          //         children: [
-                                          //           Icon(
-                                          //             Icons.remove_red_eye,
-                                          //             size: 22,
-                                          //             color: Colors.teal
-                                          //                 .withOpacity(0.7),
-                                          //           ),
-                                          //           const SizedBox(
-                                          //               width: 5),
-                                          //           Text(providerDtmNews
-                                          //               .modelDtmNews3Temp[
-                                          //           index]
-                                          //               .views
-                                          //               .toString()),
-                                          //         ],
-                                          //       )
-                                          //     ],
-                                          //   ),
-                                          // ),
-                                        ],
-                                      ),
-                                    )),
+
+                                            SizedBox(
+                                              width: MediaQuery.of(context).size.width*0.95,
+                                              child: ExpansionTile(
+                                                  shape: Border.all(color: Colors.transparent),
+                                                title: Text(listModelSocket[index].title, style: const TextStyle(fontWeight: FontWeight.bold),),
+                                                children:[ Padding(
+                                                  padding: const EdgeInsets.all(8.0),
+                                                  child: Text(listModelSocket[index].summary),
+                                                ),]
+
+                                              ),
+                                            ),
+                                            // Padding(
+                                            //   padding:
+                                            //   const EdgeInsets.fromLTRB(
+                                            //       8, 8, 8, 10),
+                                            //   child: Row(
+                                            //     crossAxisAlignment:
+                                            //     CrossAxisAlignment.center,
+                                            //     mainAxisAlignment:
+                                            //     MainAxisAlignment
+                                            //         .spaceBetween,
+                                            //     children: [
+                                            //       const Icon(
+                                            //         Icons.keyboard_arrow_down,
+                                            //       ),
+                                            //       Row(
+                                            //         children: [
+                                            //           Icon(
+                                            //             Icons.remove_red_eye,
+                                            //             size: 22,
+                                            //             color: Colors.teal
+                                            //                 .withOpacity(0.7),
+                                            //           ),
+                                            //           const SizedBox(
+                                            //               width: 5),
+                                            //           Text(providerDtmNews
+                                            //               .modelDtmNews3Temp[
+                                            //           index]
+                                            //               .views
+                                            //               .toString()),
+                                            //         ],
+                                            //       )
+                                            //     ],
+                                            //   ),
+                                            // ),
+                                          ],
+                                        ),
+                                      )),
+                            ),
                           ),
                         ),
                       ])
